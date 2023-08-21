07e30fd699d5a63cb66368b961b3de65380077c4
Aishwarya-j10 <123799540+Aishwarya-j10@users.noreply.github.com>
2023-08-21 15:07:32 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,36 +1,39 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
-@app.route('/') 
-def home(): 
-    return render_template('home.html')  
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-@app.route('/about') 
-def about(): 
-    return render_template('about.html') 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-@app.route('/skills') 
-def skills(): 
+@app.route('/skills')
+def skills():
     return render_template('skills.html')
 
-@app.route('/project') 
-def project(): 
+@app.route('/project')
+def project():
     return render_template('project.html')
 
-@app.route('/contact', methods = ["GET", "POST"])  
-def contact(): 
-    if request.method == 'POST' : 
+@app.route('/contact', methods = ["GET", "POST"])
+def contact():
+    if request.method == 'POST' :
         name = request.form['name']
-        email = request.form['email'] 
+        email = request.form['email']
         subject = request.form['subject']
-        message = request.form['message'] 
+        message = request.form['message']
 
-        print(f'Name: {name}, Email:{email}, Message: {message}') 
+        print(f'Name: {name}, Email:{email}, Message: {message}')
 
-        return render_template('contact.html', message_sent = True) 
-    return render_template('contact.html', message_Sent = False) 
+        with open('message.txt', 'a') as file:
+            file.write(f"Name : {name}, Email : {email}, Subject : {subject}, Message : {message}\n")
+
+        return render_template('contact.html', message_sent = True)
+    return render_template('contact.html', message_Sent = False)
 
 
-if __name__ == '__main__' : 
-    app.run(debug = True) 
+if __name__ == '__main__' :
+    app.run(debug = True)
